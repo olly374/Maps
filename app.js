31d1ad83f9662c84a183b087c8cbefe6d6194ec0
olly374 <31632996+olly374@users.noreply.github.com>
2017-09-14 22:04:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,35 +17,31 @@ var code = '1SIU5KR29zNm52A9GtF137Y8Juire76G58Jzs75-bYfw'
     callback: function(sheet, tabletop){ 
       
       for (var i in sheet){
-	    var data = sheet[i];
-		  var icon = L.icon(
-	  
-	      for (var i in sheet){
-            var data = sheet[i];
+        var data = sheet[i];
 
-              var icon = L.icon({
-                  iconUrl: data.icon,
-                  iconSize:     [30, 30], // size of the icon
-                  iconAnchor:   [26, 60], // point of the icon which will correspond to marker's location
-                  popupAnchor: [0, -60]
+          var icon = L.icon({
+              iconUrl: data.icon,
+              iconSize:     [30, 60], // size of the icon
+              iconAnchor:   [26, 60], // point of the icon which will correspond to marker's location
+              popupAnchor: [0, -60]
+          });
+          if (data.iconori === "left") {
+            icon = L.icon({
+              iconUrl: data.icon,
+              iconSize:     [60, 52], 
+              iconAnchor:   [60, 26], 
+              popupAnchor: [-35, -26]
               });
-              if (data.iconori === "left") {
-                icon = L.icon({
-                  iconUrl: data.icon,
-                  iconSize:     [60, 60], 
-                  iconAnchor:   [60, 26], 
-                  popupAnchor: [-35, -26]
-                  });
-              };
-              if (data.iconori === "right") {
-                icon = L.icon({
-                  iconUrl: data.icon,
-                  iconSize:     [90, 90], 
-                  iconAnchor:   [0, 26], 
-                  popupAnchor: [35, -26]
-                  })
-                };
-		}
+          };
+          if (data.iconori === "right") {
+            icon = L.icon({
+              iconUrl: data.icon,
+              iconSize:     [60, 52], 
+              iconAnchor:   [0, 26], 
+              popupAnchor: [35, -26]
+              })
+            };
+
           L.marker([data.longitude, data.latitude], {icon: icon})
           .addTo(map)
           .bindPopup("<strong style='color: #84b819'>" + data.newsroom + "</strong><br>" + 
