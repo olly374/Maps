840ff7e2e453fd5584f284b43dbc72f0df3139fe
olly374 <31632996+olly374@users.noreply.github.com>
2017-09-14 22:44:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,15 +18,37 @@ var code = '1SIU5KR29zNm52A9GtF137Y8Juire76G58Jzs75-bYfw'
       
       for (var i in sheet){
         var data = sheet[i];
-		   L.circle([data.longitude, data.latitude], {
-			   color: '#FFF',
-			   fill: true,
-			   fillColor: '#FFF',
-			   fillOpacity: 1,
-			   radius: 25000,
-			   opacity: 1,
-			   weight: 0
-			}) // radius of circle in metres. Don't use circleMarker, as radius is in pixels
+		   if (data.iconori === "left") {
+			L.circle([data.longitude, data.latitude], {
+				color: '#FFF',
+			    fill: true,
+			    fillColor: '#FFF',
+			    fillOpacity: 1,
+			    radius: 5000,	// radius of circle in metres. Don't use circleMarker, as radius is in pixels
+			    opacity: 1,
+			    weight: 0
+			})}
+		    if (data.iconori === "right") {
+			 L.circle([data.longitude, data.latitude], {
+				color: '#AAA',
+			    fill: true,
+			    fillColor: '#AAA',
+			    fillOpacity: 1,
+			    radius: 5000,	// radius of circle in metres. Don't use circleMarker, as radius is in pixels
+			    opacity: 1,
+			    weight: 0
+			})}
+			if (data.iconori === "above") {
+			 L.circle([data.longitude, data.latitude], {
+				color: '#CCC',
+			    fill: true,
+			    fillColor: '#CCC',
+			    fillOpacity: 1,
+			    radius: 5000,	// radius of circle in metres. Don't use circleMarker, as radius is in pixels
+			    opacity: 1,
+			    weight: 0
+			})}
+					   
           .addTo(map)
           .bindPopup("<strong style='color: #84b819'>" + data.newsroom + "</strong><br>" + data.company + " | " + data.city + "<br>Head: " + data.head).openPopup();
       }
